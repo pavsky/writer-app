71238b2fe8bb551fc0723e91654883665e36ac99
Pawel Betkowski <46534823+pavsky@users.noreply.github.com>
2021-07-17 18:35:16 +0100

    # First version done
    Writing to files, displaying file lists

--- renderer.js
+++ renderer.js
@@ -1,4 +1,11 @@
 // Toggling Menu
+const fs = require('fs-extra')
+const readline = require('readline');
+const $ = require('jquery')
+
+// Testing
+// $('#dashboard').hide();
+// $('#file-screen').show();
 
 $('#open-menu, #close-menu').on('click', () => {
     toggleMenu();
@@ -22,3 +29,54 @@ const toggleHelp = () => {
         width: "toggle"
       });
 }
+
+// Show all files
+const showAllFiles = () => {
+    const fileNames = fs.readdirSync('./files/');
+
+    async function getFirstLine(filePath) {
+        const fileContent = await fs.readFile(filePath, 'utf-8');
+        return (fileContent.match(/(^.*)/) || [])[1] || '';
+    } 
+    
+    for(i = 0; i < fileNames.length; i++) {
+        let temporaryLines = [];
+        let newFileButton = document.createElement('button');
+        let newPath = './files/' + fileNames[i];
+        let rd = readline.createInterface({
+            input: fs.createReadStream(newPath),
+            output: process.stdout,
+            console: false
+        });
+        rd.on('line', function(line) {
+            temporaryLines.push(line);
+            $(newFileButton).html(temporaryLines[0]);
+        });
+        $(newFileButton).addClass('file-button');
+        $('.content').append(newFileButton);
+    }
+}
+
+showAllFiles();
+
+$('#add-new-file').on('click', () => {
+    $('#dashboard').fadeOut();
+    $('#file-screen').delay(350).fadeIn();
+})
+
+const saveFile = (fileTitle, bodyText) => {
+    const contents = '<h3>' + $(fileTitle).val() + '</h3>' + '<p>' + $(bodyText).text() +'</p>';
+    fs.writeFile('./files/' + $(fileTitle).val()  + '.html', contents);
+}
+
+$('#save-file').on('click', () => {
+    saveFile('#input-title', '#input-body');
+})
+
+$('#menu-link-files').on('click', () => {
+    $('#file-screen').fadeOut();
+    $('#dashboard').delay(350).fadeIn();
+    toggleMenu();
+    $('.file-button').remove();
+    showAllFiles();
+})
